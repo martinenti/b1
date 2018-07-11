248e72b6665f6681920f97fd9ef21df750585ec7
martinenti <40662211+martinenti@users.noreply.github.com>
2018-07-12 01:01:44 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.user.setGame(`_bc RGM CLAN `,"http://twitch.tv/S-F")
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "S Bot";
+    let copy = "RGM BC Bot";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
